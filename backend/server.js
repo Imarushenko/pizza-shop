72acefa9a616772c9b1d2a0edd2b66bc37586a06
Igor Marushchenko <imarushchenko@allot.com>
2023-11-15 13:15:04 +0200

    create user routes

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import connectDB from "./config/db.js";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
@@ -10,9 +11,12 @@ const port = process.env.PORT || 3001;
 connectDB();
 const app = express();
 
-// Body parser middleware
+// body parser middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// cookie parser middleware
+app.use(cookieParser());
 
 app.get("/", async (req, res) => {
   res.send("API is running...");
