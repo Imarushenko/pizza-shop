929e21d197a243db364714664e598227b2086384
Igor Marushchenko <imarushchenko@allot.com>
2023-12-29 11:39:25 +0200

    disable categories and add pagination to the home screen and the product list of the admin panel

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,41 +1,20 @@
-// frontend/homepagescreen.js
-import React from "react";
-import { Col, Row } from "react-bootstrap";
-import { useDispatch, useSelector } from "react-redux";
-import { setCategory } from "../slices/categorySlice";
-import Product from "../components/Product";
+import { Row, Col } from "react-bootstrap";
+import { useParams } from "react-router-dom";
 import { useGetProductsQuery } from "../slices/productsApiSlice";
+import Product from "../components/Product";
 import Loader from "../components/Loader";
 import Message from "../components/Message";
+import Paginate from "../components/Paginate";
 
 const HomeScreen = () => {
-  const { data: products, isLoading, error } = useGetProductsQuery();
-  const dispatch = useDispatch();
-  const selectedCategory = useSelector((state) => state.category);
+  const { pageNumber } = useParams();
 
-  const handleCategoryChange = (category) => {
-    dispatch(setCategory(category));
-  };
+  const { data, isLoading, error } = useGetProductsQuery({
+    pageNumber,
+  });
 
   return (
     <>
-      {/* Add category tabs */}
-      <div className="category-tabs">
-        {["Pizza", "Salad", "Sambusak", "Specials"].map((category) => (
-          <div
-            key={category}
-            className={`category-tab ${
-              selectedCategory.toLowerCase() === category.toLowerCase()
-                ? "active"
-                : ""
-            }`}
-            onClick={() => handleCategoryChange(category)}
-          >
-            {category}
-          </div>
-        ))}
-      </div>
-
       {isLoading ? (
         <Loader />
       ) : error ? (
@@ -45,18 +24,13 @@ const HomeScreen = () => {
       ) : (
         <>
           <Row>
-            {products
-              .filter(
-                (product) =>
-                  product.category.toLowerCase() ===
-                  selectedCategory.toLowerCase()
-              )
-              .map((product) => (
-                <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                  <Product product={product} />
-                </Col>
-              ))}
+            {data.products.map((product) => (
+              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                <Product product={product} />
+              </Col>
+            ))}
           </Row>
+          <Paginate pages={data.pages} page={data.page} />
         </>
       )}
     </>
@@ -64,3 +38,70 @@ const HomeScreen = () => {
 };
 
 export default HomeScreen;
+
+// // frontend/homepagescreen.js
+// import React from "react";
+// import { Col, Row } from "react-bootstrap";
+// import { useDispatch, useSelector } from "react-redux";
+// import { setCategory } from "../slices/categorySlice";
+// import Product from "../components/Product";
+// import { useGetProductsQuery } from "../slices/productsApiSlice";
+// import Loader from "../components/Loader";
+// import Message from "../components/Message";
+
+// const HomeScreen = () => {
+//   const { data: products, isLoading, error } = useGetProductsQuery();
+//   const dispatch = useDispatch();
+//   const selectedCategory = useSelector((state) => state.category);
+
+//   const handleCategoryChange = (category) => {
+//     dispatch(setCategory(category));
+//   };
+
+//   return (
+//     <>
+//       {/* Add category tabs */}
+//       <div className="category-tabs">
+//         {["Pizza", "Salad", "Sambusak", "Specials"].map((category) => (
+//           <div
+//             key={category}
+//             className={`category-tab ${
+//               selectedCategory.toLowerCase() === category.toLowerCase()
+//                 ? "active"
+//                 : ""
+//             }`}
+//             onClick={() => handleCategoryChange(category)}
+//           >
+//             {category}
+//           </div>
+//         ))}
+//       </div>
+
+//       {isLoading ? (
+//         <Loader />
+//       ) : error ? (
+//         <Message variant="danger">
+//           {error?.data?.message || error.error}
+//         </Message>
+//       ) : (
+//         <>
+//           <Row>
+//             {products
+//               .filter(
+//                 (product) =>
+//                   product.category.toLowerCase() ===
+//                   selectedCategory.toLowerCase()
+//               )
+//               .map((product) => (
+//                 <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+//                   <Product product={product} />
+//                 </Col>
+//               ))}
+//           </Row>
+//         </>
+//       )}
+//     </>
+//   );
+// };
+
+// export default HomeScreen;
--- frontend/src/slices/categorySlice.js
+++ frontend/src/slices/categorySlice.js
@@ -1,14 +1,14 @@
-import { createSlice } from "@reduxjs/toolkit";
+// import { createSlice } from "@reduxjs/toolkit";
 
-const categorySlice = createSlice({
-  name: "category",
-  initialState: "pizza",
-  reducers: {
-    setCategory: (state, action) => {
-      return action.payload;
-    },
-  },
-});
+// const categorySlice = createSlice({
+//   name: "category",
+//   initialState: "pizza",
+//   reducers: {
+//     setCategory: (state, action) => {
+//       return action.payload;
+//     },
+//   },
+// });
 
-export const { setCategory } = categorySlice.actions;
-export default categorySlice.reducer;
+// export const { setCategory } = categorySlice.actions;
+// export default categorySlice.reducer;
